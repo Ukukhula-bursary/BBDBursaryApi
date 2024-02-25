80c95c1d82576622fbecf353d40a9a349b68ad0f
Manqoba-stack <manyoba1997@gmail.com>
2024-02-25 17:36:14 +0200

    new request header

---
+++ src/main/java/com/ukukhula/bursaryapi/entities/Request/UserRequest.java
@@ -0,0 +1,37 @@
+package com.ukukhula.bursaryapi.entities.Request;
+
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import com.ukukhula.bursaryapi.entities.Contact;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class  UserRequest {
+    // private int id;
+    private String firstName;
+    private String lastName;
+  
+    // private int userRoleId;
+    // private int contactId;
+    private String PhoneNumber;
+    private String Email;
+    private int IsActiveID;
+
+    public UserRequest(String firstName, String lastName,String phoneNumber ,String email , int IsActiveID) {
+        // this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Email=email;
+        this.PhoneNumber=phoneNumber;
+        this.IsActiveID = IsActiveID;
+    }
+}
+
